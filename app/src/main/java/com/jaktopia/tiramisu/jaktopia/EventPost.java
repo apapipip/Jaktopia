191ef9098bea5a95e2cd7c79f95cacadab08c3be
Luthfi Firmansyah Soehadak <lsoehadak@Luthfis-MacBook-Pro.local>
2017-02-16 17:50:06 +0700

    no message

---
+++ app/src/main/java/com/jaktopia/tiramisu/jaktopia/EventPost.java
@@ -0,0 +1,19 @@
+package com.jaktopia.tiramisu.jaktopia;
+
+/**
+ * Created by lsoehadak on 2/16/17.
+ */
+
+public class EventPost {
+    int id;
+    string name;
+    string locationName;
+    string category;
+    string categoryIconUrl;
+    string userPhotoUrl;
+    int favoriteCount;
+
+    public EventPost() {
+
+    }
+}
